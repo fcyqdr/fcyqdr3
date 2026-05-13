6f48d4abdfb3ee7a7d08ffec519634fbf5208e83
fcyqdr <fcyqdr1@gmail.com>
2026-05-14 03:34:32 +0800

    创建 config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+/* 
+   SHADOW SENTINEL - 小黑巷动态控制台 
+   以后换链接只需要改这个文件，不用动 HTML 代码
+*/
+const SHADOW_DATA = {
+    // 导航卡片配置
+    tiles: [
+        { id: "tile1", url: "http://vavc.cc", title: "偸凊羣茭" },
+        { id: "tile2", url: "http://wv24.icu", title: "孧囡囉莉" },
+        { id: "tile3", url: "http://www.avaw.cc", title: "嫃亾矗譒" },
+        { id: "tile4", url: "http://se678.top", title: "咸氵显视频" },
+        { id: "tile5", url: "http://a.av66.sbs", title: "​【露天激战】" },
+        { id: "tile6", url: "http://www.vu34.cc", title: "多人狂欢" }
+    ],
+    // 底部二维码配置
+    backups: {
+        short: "https://828.cx",
+        old: "https://av66.sbs",
+        permanent: "https://fcyqdr.github.io/fcyqdr"
+    },
+    // 站长动态（会滚动显示）
+    signatures: [
+        "深夜不打烊，快乐不停歇",
+        "资源每日更新，质量有保证",
+        "小黑巷，懂你的夜",
+        "影子哨兵，离岸支撑"
+    ]
+};
